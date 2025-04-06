dd91b51402d5cd7c5bffd28c89c00e222b5a2f94
Brendan Walsh <154637324+bwalsh10@users.noreply.github.com>
2025-04-06 15:44:43 -0400

    Create Assignment 11.R

---
+++ Assignment 11.R
@@ -0,0 +1,23 @@
+#original function#
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } return(outlier.vec) }
+
+#first revision: move the return function to its own line#
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } 
+  return(outlier.vec) }
+
